569e9e0b9ab43891e05f64c7670cdd0781e6815e
Ian Carroll <icarroll@sesync.org>
2017-07-17 22:56:03 -0400

    update worksheet

--- worksheet.sh
+++ worksheet.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 ## Configure git
-
 git config --global user.name ...
 git config --global user.email ...
 
 ## Change the "origin" remote URL and push
 git remote set-url origin ...
-git push --all
+
+## Set the SESYNC-CI repository upstream and pull updates
+git remote add upstream https://github.com/sesync-ci/handouts.git
+git pull upstream master
