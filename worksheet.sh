13da3c3f50e5fdc2c1b228a4a76a1384ca5ee969
Ian Carroll <icarroll@sesync.org>
2018-08-03 14:05:33 -0400

    fixes #3 and adds gitignore

--- worksheet.sh
+++ worksheet.sh
@@ -5,6 +5,6 @@ git config --global user.email ...
 git commit --no-edit --amend --reset-author
 
 # link your local repository to the origin repository on GitHub
+# paste and run the code shown on your GitHub repo under the heading:
+# "…or push an existing repository from the command line"
 
-git remote add origin ...
-git push -u origin master
